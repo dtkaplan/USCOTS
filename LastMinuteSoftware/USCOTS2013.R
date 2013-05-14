817c340cfb78a1ade4a62eb3e45c4d25c9d411d6
Daniel Kaplan <dtkaplan@gmail.com>
2013-05-14 13:18:36 -0400

    Rmarkdown notes

--- LastMinuteSoftware/USCOTS2013.R
+++ LastMinuteSoftware/USCOTS2013.R
@@ -2,7 +2,9 @@
 fetchRmd <- function(name) {
   fetchAddress="http://www.mosaic-web.org/go/datasets/"
   fullname = paste(fetchAddress,name,sep="")
-  tmpName = tempfile(pattern="template",fileext=".Rmd")
+  tmpName = tempfile(pattern=paste(basename(name),"-",sep=""),fileext=".txt",tmpdir=getwd())
   download.file(fullname,tmpName)
   file.edit(tmpName)
+  Sys.sleep(2)
+  on.exit(file.remove(tmpName))
 }
